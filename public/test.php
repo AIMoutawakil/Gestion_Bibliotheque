d16b6658ed9f361d4c3fcdd5e20c86376284c7a8
DevMoutawakil <massarmostapha332@gmail.com>
2026-07-21 21:53:45 +0100

    Clean up test.php by removing comments
    Removed commented lines and cleaned up code.

--- public/test.php
+++ public/test.php
@@ -1,10 +1,7 @@
 <?php
-// public/test.php
 
 require_once '../src/Controller/BookController.php';
 
-// On instancie le Controller
 $controller = new BookController();
 
-// On appelle la méthode qui génère le JSON
 $controller->getAllBooks();
